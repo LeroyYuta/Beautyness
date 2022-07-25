42d562417ff53e2b6e3fc5468e44d027294e3d0f
Alexandra Volkova <pushnaya666@gmail.com>
2022-07-25 15:33:05 +0300

    burger add, check fsz

--- js/first_section.js
+++ js/first_section.js
@@ -31,8 +31,20 @@ if (animItems.length > 0) {
 
     setTimeout( () => {
         animOnScroll();
-    }, 500);
-   
-   
-    
+    }, 500);   
 }
+
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+
+    });
+
+} () );
