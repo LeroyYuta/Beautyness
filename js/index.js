69a3d116d43b003ab506673fe064b4103bb87d44
kondrusina <kondrusina@rambler.ru>
2022-07-27 22:38:47 +0300

    change js file

--- js/index.js
+++ js/index.js
@@ -1,18 +1,2 @@
-//Latest Updates
-const btn = document.querySelector ('.button__btn');
-const btn_2 = document.querySelector ('.button__btn-2');
-let block = document.querySelector(".wrapper-2");
-function viewBox() {
-    block.style.display = "block";
-    if (block.style.display === "block") {
-        btn.style.display = "none";
-        btn_2.innerHTML = "Hide";
-        block.style.marginTop = "2%";
-    }
-}
-function hideBox() {
-    block.style.display = "none"
-    btn.style.display = "block";
-}
 
 
