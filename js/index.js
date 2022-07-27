064bb2924f715efa91b350b7a568043f2cc7cae5
KristinaSolo <97984146+KristinaSolo@users.noreply.github.com>
2022-07-27 11:18:22 +0300

    img/js

--- js/index.js
+++ js/index.js
@@ -25,4 +25,23 @@ function change(identifier) {
 
 
 ///
+   const imgs =document.querySelectorAll('.box-img');
+
+   for(i = 0; i < imgs.length; i++){
+    const img = imgs[i] ;
+    img.addEventListener('mousemove', startRotate); //запускает функцию вращения
+    img.addEventListener('mouseout', stopRotate); // останавливает функцию вращения
+   }
    
+   
+   function startRotate(event){
+const boxImg =  this.querySelector('.img');
+const halfHei = boxImg.offsetHeight / 2; // нашли середину опираясь на высоту и разделили пополам
+
+boxImg.style.transform ='rotateX('+-(event.offsetY - halfHei)/10+'deg) rotateY('+(event.offsetX - halfHei) /10+'deg)'
+   } // deg (градус)
+
+   function stopRotate(event){
+    const boxImg =  this.querySelector('.img');
+    boxImg.style.transform ='rotate(0)';   // сброс всех вычеслений
+       }
