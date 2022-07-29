1898f798dc22750db12dfdea84c22399ab84b98f
Alexandra Volkova <pushnaya666@gmail.com>
2022-07-29 16:04:18 +0300

    correct code

--- js/first_section.js
+++ js/first_section.js
@@ -1,3 +1,4 @@
+// Header animation
 const animItems = document.querySelectorAll('._anim-items');
 
 if (animItems.length > 0) {
@@ -35,17 +36,26 @@ if (animItems.length > 0) {
 }
 
 
+// Burger handler
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav_active');
     });
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__nav_active');
-
     });
+
+    if (window.innerWidth <= 979) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header__nav_active');
+            });
+        }
+    }
 
 } () );
 
